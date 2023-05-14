cff134fe434d39b907c6c37a02882d37a2e04133
Murat Oralbaev <successmanonly@gmail.com>
2023-05-14 21:52:41 +0300

    SavingAccount.java modofied

--- src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
@@ -17,4 +17,57 @@ public class CreditAccountTest {
 
         Assertions.assertEquals(3_000, account.getBalance());
     }
+//    @Test
+//    public void shouldAddNone(){
+//        CreditAccount account = new CreditAccount(
+//                0,
+//                5_000,
+//                15
+//        );
+//        account.add(0);
+//        Assertions.assertEquals(0,account.getBalance());
+//    }
+//    @Test
+//    public void shouldAddToNegativeBalance(){
+//        CreditAccount account = new CreditAccount(
+//                0,
+//                5_000,
+//                15
+//        );
+//        account.add(-3_000);
+//        Assertions.assertEquals(0,account.getBalance());
+//    }
+//    @Test
+//    public void shouldPayAboveCreditLimit(){
+//        CreditAccount account = new CreditAccount(
+//                0,
+//                5_000,
+//                15
+//        );
+//        account.pay(6_000);
+//        Assertions.assertEquals(0, account.getBalance());
+//    }
+//    @Test
+//    public void shouldPayWithinCreditLimit(){
+//        CreditAccount account = new CreditAccount(
+//                0,
+//                5_000,
+//                15
+//        );
+//        account.pay(3_000);
+//        Assertions.assertEquals(-3_000, account.getBalance());
+//    }
+//    @Test
+//    public void test2() {
+//        CreditAccount account = new CreditAccount(
+//                0,
+//                5_000,
+//                15
+//        );
+//        account.();
+//        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+//            account.();
+//        });
+//    }
+
 }
--- src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
@@ -18,4 +18,60 @@ public class SavingAccountTest {
 
         Assertions.assertEquals(2_000 + 3_000, account.getBalance());
     }
+    @Test
+    public void shouldAddNone(){
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+        account.add(0);
+        Assertions.assertEquals(2_000,account.getBalance());
+    }
+    @Test
+    public void shouldAddMoreThanMaxBalance(){
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+        account.add(11_000);
+        Assertions.assertEquals(2_000, account.getBalance());
+    }
+    @Test
+    public void shouldPayLessThanMinBalance(){
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+        account.pay(900);
+        Assertions.assertEquals(2_000 - 900, account.getBalance());
+    }
+    @Test
+    public void shouldPayNone(){
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+        account.pay(0);
+        Assertions.assertEquals(2_000, account.getBalance());
+    }
+    @Test
+    public void shouldPayMoreThanBalance(){
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+        account.pay(3_000);
+        Assertions.assertEquals(2_000, account.getBalance());
+    }
+
 }
