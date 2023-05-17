08df08c7f2efcfe6664ec858ee77b857a0c929ac
Murat Oralbaev <successmanonly@gmail.com>
2023-05-17 22:57:37 +0300

    CreditAccount changed

--- src/main/java/ru/netology/javaqadiplom/CreditAccount.java
+++ src/main/java/ru/netology/javaqadiplom/CreditAccount.java
@@ -95,10 +95,12 @@ public class CreditAccount extends Account {
      */
     @Override
     public int yearChange() {
-
-        return balance / 100 * rate;
+        if (balance >= 0) {
+            return 0;
+        } else {
+            return -(balance * rate) / 100;
+        }
     }
-
 
 
     public int getCreditLimit() {
